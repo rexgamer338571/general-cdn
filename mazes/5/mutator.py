34a641f34f1303f82238bd720986836d51969d9d
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-04-26 19:46:33 +0200

    Create mutator.py

---
+++ mazes/5/mutator.py
@@ -0,0 +1,33 @@
+import sys
+
+inputstring = """ _________
+|___  |   |
+| |  _| |_|
+|  _|___  |
+| |_   _  |
+|_____|___|"""
+
+inputstring = [sys.argv[1] if sys.argv[1] else inputstring]
+
+def convert(multiline_str):
+    lines = multiline_str.split('\n')
+    
+    first_line = lines[0].strip() + '|'
+
+    processed_lines = [first_line]
+
+    for line in lines[1:]:
+        processed_lines.append(line.replace('_', ' '))
+        processed_lines.append('|' + line.replace('|', ' ')[1:-1] + '|')
+
+    second_line_length = len(lines[1])
+
+    processed_lines[-1] = '|' + ' ' * (second_line_length - 2) + '|'
+
+    last_line = ' ' + '_' * (second_line_length - 2) + ' '
+    processed_lines.append(last_line)
+
+    return '\n'.join(processed_lines)
+
+
+print(convert(inputstring))
